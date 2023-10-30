fd56c70f9230f081753fb1d5de4846e1e0de88f9
alicanov98 <alicanov1998@gmail.com>
2023-10-30 14:19:11 +0400

    add  animations

--- src/component/Experience.jsx
+++ src/component/Experience.jsx
@@ -1,17 +1,48 @@
 /* eslint-disable react/no-unknown-property */
-import { OrbitControls } from "@react-three/drei"
-import { Avatar } from "./Avatar"
-
+import {
+  ContactShadows,
+  Environment,
+  OrbitControls,
+  Sky,
+} from "@react-three/drei";
+import { Avatar } from "./Avatar";
+import { useControls } from "leva";
 
 export const Experience = () => {
+  const { animation } = useControls({
+    animation: {
+      value: "Typing",
+      options: ["Typing", "Standing", "Falling"],
+    },
+  });
+
   return (
     <>
-    <OrbitControls/>
+      <OrbitControls />
+      <Sky />
+      <Environment preset="sunset" />
       <group position-y={-1}>
-        <Avatar/>
+        <ContactShadows
+          opacity={0.42}
+          scale={10}
+          blur={1}
+          far={10}
+          resolution={256}
+          color="#000000"
+        />
+        <Avatar animation={animation} />
+        {animation === "Typing" && (
+          <mesh scale={[0.7, 0.4, 0.7]} position-y={0.25}>
+            <boxGeometry />
+            <meshStandardMaterial color="white" />
+          </mesh>
+        )}
+        <mesh scale={5} rotation-x={-Math.PI * 0.5} position-y={-0.001}>
+          <planeGeometry />
+          <meshStandardMaterial color="white" />
+        </mesh>
+        {/* <ambientLight intensity={1} /> */}
       </group>
-      <ambientLight intensity={1} />
-
     </>
-  )
-}
+  );
+};
